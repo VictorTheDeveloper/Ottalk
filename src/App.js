71c585a2aa92c2ee41cbb29b7be580556724639e
Victor Costa <vckundrotas@gmail.com>
2022-01-05 22:16:56 -0300

    aditando app.js

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+export default () => {
+    return (
+        <div className="app-window">
+            <div className="sidebar">
+
+                <header>
+                    <img src="" alt="" />
+                </header>
+            
+                <div className="search">
+                    ...
+                </div>
+                <div className="chatlist">
+                    ...
+                </div>
+            </div>
+            <div className="contentarea">
+            ...
+            </div>
+        </div>
+    );
+}
